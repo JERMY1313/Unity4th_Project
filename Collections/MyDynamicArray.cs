787d156188b159dde04e2f9009c8e0e5be13de0a
JERMY1313 <67793606+JERMY1313@users.noreply.github.com>
2023-10-06 18:07:11 +0900

    Inventory
    -Inventory
    -DynamicArray

---
+++ Collections/MyDynamicArray.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//PsacalCase : 사용자정의 자료형 이름, 함수 이름, 프로퍼티 이름, public/protected 멤버 변수 이름
+//camelCase : 지명변수 (unity API 활용할때는 public/protected 멤버 변수 이름도 이렇게 주로 씀
+//_camelCase : private 멤버 변수 이름
+//snake_case : 상수 정의시
+//UPPER_CASE : 상수 정의시
+//m_Hungarian, iNum, fX <- 요즘날에는 잘 사용하지 않으나 가끔 사용. 사용 예 : static 멤버변수,  s_Instance
+
+namespace Collections
+{
+
+    internal class MyDynamicArray
+    {
+        public object this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException();
+
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException();
+
+                _items[index] = value;
+            }
+        }
+
+        // Count => _count; 는 
+        //public int Count
+        //{
+        //   get
+        //   {
+        //      return _count;
+        //   }
+        //를 줄인 문장이다
+        public int Count => _count;
+
+        public int Capacity => _items.Length;
+
+        private int _count;
+        private const int DEFAULT_SIZE = 1;
+        private object[] _items = new object[DEFAULT_SIZE];
+
+        public void Add(object item)
+        {
+            if (_count >= _items.Length)
+            {
+                {
+                    object[] tmp = new object[_count * 2];
+                    Array.Copy(_items, tmp, _count);
+                    _items = tmp;
+                }
+
+                _items[_count++] = item;
+            }
+        }
+            public object? Find(Predicate<object> match) //FInd = 물건을 찾고싶을때
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    if (match(_items[i]))
+                        return _items[i];
+                }
+                return default;
+            }
+
+            public int FindIndex(Predicate<object> match) //FindIndex = 정보를 찾아서 수정하고싶을때
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    if (match(_items[i]))
+                        return i;
+                }
+                return -1;
+            }
+            public bool Contains(object item)
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    if (_items[i] == item)
+                        return true;
+                }
+
+                return false;
+            }
+            public void RemoveAt(int index)
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException();
+
+                for (int i = 0; i < _count - 1; i++)
+                {
+                    _items[i] = _items[i * 1];
+                }
+                _count--;
+            }
+
+            public bool Remove(object item)
+            {
+                int index = FindIndex(X => X == item);
+
+                if (index < 0)
+                    return false;
+
+                RemoveAt(index);
+                return true;
+            }
+        }
+    }
---
+++ Collections/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Security.Principal;
+
+namespace Collections
+{
+    enum ItemID
+    {
+        RedPotion = 20,
+        BluePotion = 21,
+    }
+    class SloatData : IComparable<SloatData>
+    {
+        public bool isEmpty => id == 0 && num == 0;
+
+        public int id;
+        public int num;
+
+        public SloatData(int id, int num)
+        {
+            this.id = id;
+            this.num = num;
+        }
+
+        public int CompareTo(SloatData? other)
+        {
+            return this.id == other?.id && this.num == other?.num? 0: 1;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDynamicArray Inventory = new MyDynamicArray();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Inventory.Add(new SloatData(0, 0));
+
+                Console.WriteLine($"슬롯 {i} : [{(ItemID)((SloatData)Inventory[i]).id}], [{((SloatData)Inventory[i]).num}]");
+            }
+
+            Inventory[0] = new SloatData((int)ItemID.RedPotion, 40);
+            Inventory[1] = new SloatData((int)ItemID.BluePotion, 99);
+            Inventory[2] = new SloatData((int)ItemID.BluePotion, 40);
+
+            Console.WriteLine($"인벤토리 정보 :");
+            for(int i = 0; i< Inventory.Count; i++)
+            {
+                Console.WriteLine($"슬롯 {i} : [{(ItemID)((SloatData)Inventory[i]).id}], [{((SloatData)Inventory[i]).num}]");
+            }
+            //todo -> 파란포션 5개를 획득
+            //1. 파란포션 5개가 들어갈 수 있는 슬롯을 찾아야함.
+            int availableSloatIndex = Inventory.FindIndex(sloatData => ((SloatData)sloatData).isEmpty || (((SloatData)sloatData).id == (int)ItemID.BluePotion && ((SloatData)sloatData).num <= 99 - 5));
+            //2. 해당 슬롯의 아이템 갯수에다가 내가 추가하려는 갯수를 더한 만큼의 수정예상값을 구함
+            int expected = ((SloatData)Inventory[availableSloatIndex]).num + 5;
+            //3. 새로운 아이템 데이터를 만들어서 슬롯데이터를 교체해줌
+            SloatData newSloatData = new SloatData((int)ItemID.BluePotion, expected);
+            Inventory[availableSloatIndex] = newSloatData;
+
+
+            MyDynamicArray<SloatData> inventory2 = new MyDynamicArray<SloatData>();
+            //숙제 해올것.
+
+            /*
+            inventory[1] = "철수";
+            Console.WriteLine($"동적배열의 0번째 인덱스 값 : {inventory[0]}");
+
+            int idx = inventory.FindIndex(x => (string)x == "철수");
+            idx = inventory.FindIndex(x => (int)x > 3);
+
+            int 파란포션5개추가가능한인덱스 = inventory.FindIndex(x => x.isEmpty == true ||
+                                                                (x.id == 파란포션아이디 &&
+                                                                 x.num <= 파란포션최대갯수 - 5))
+            int 수정될갯수 = inventory[파란포션5개추가가능한인덱스].num + 5;
+            슬롯데이터 수정될슬롯데이터 = new 슬롯데이터(파란포션아이디, 수정될갯수);
+            inventory[파란포션5개추가가능한인덱스] = 수정될슬롯데이터;
+            */
+        }
+    }
+}
